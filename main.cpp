3c39e60a1475b6e368995772ec54bb765c9d1b84
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-01 13:30:09 +0300

    added construct()

--- main.cpp
+++ main.cpp
@@ -2,16 +2,22 @@
 
 void destroy(int ** mtx, size_t created);
 int ** create(size_t rows, size_t cols);
+void construct(int ** mtx, int init, size_t rows, size_t cols);
 
 int main()
 {
+  size_t rows = 0;
+  size_t cols = 0;
+  std::cin >> rows >> cols;
   int ** matrix = nullptr;
   try {
-    matrix = create(5, 5);
+    matrix = create(rows, cols);
   } catch (const std::bad_alloc & e) {
     std::cerr << e.what() << "\n";
     return 1;
   }
+  construct(matrix, 2, rows, cols);
+  std::cout << matrix[0][0] << "\n";
   destroy(matrix, 5);
 }
 
@@ -36,4 +42,13 @@ int ** create(size_t rows, size_t cols)
     throw;
   }
   return mtx;
+}
+
+void construct(int ** mtx, int init, size_t rows, size_t cols)
+{
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      mtx[i][j] = init;
+    }
+  }
 }
