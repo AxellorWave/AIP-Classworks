d08d7820b083ed78845736f27cb5e389ba9946ab
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-08 13:22:50 +0300

    update task3.cpp

--- AIP-08-11-2025/task3.cpp
+++ AIP-08-11-2025/task3.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
+
+int det(const int * a, size_t n)
+{
+  if (!n) {
+    return 0;
+  }
+  if (n == 1) {
+    return a[0];
+  }
+  if (n == 2) {
+    return a[0] * a[3] - a[1] * a[2];
+  }
+  return 1; 
+}
+
 int main()
 {}
