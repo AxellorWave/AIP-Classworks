c5039286fa3c1257bb930162fc111de4283f1fa6
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-15 12:59:29 +0300

    task2 fix

--- AIP-15-11-2025/task2.cpp
+++ AIP-15-11-2025/task2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-void mul(int * c, const int * a, size_t n1, size_t m1, const int b, size_t m2)
+void mul(int * c, const int * a, size_t n1, size_t m1, const int * b, size_t m2)
 {
   for (size_t i = 0; i < n1; ++i) {
     for (size_t j = 0; j < m2; ++j) {
       int res = 0;
       const int * row_a = a + i * m1;
-      const int * row_b = a + j;
+      const int * row_b = b + j;
       for (size_t l = 0; l < m1; ++l) {
         int row_a_l = row_a[l];
         int row_b_l = row_b[l * m2];
