d1bc92a315c4b503e5cc96e4da9fb7f368deec81
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-24 16:30:57 +0300

    added countYear

---
+++ AIP-24-11-2025/task1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct Stud {
+  const char * fst;
+  const char * snd;
+  int year;
+  int dep;
+};
+
+size_t countYear(const Stud * const * studs, size_t k, int y)
+{
+  size_t r = 0;
+  for (size_t i = 0; i < k; ++i) {
+    const Stud * stud_i = studs[i];
+    int stud_y = stud_i -> year;
+    r += (stud_y == y);
+  }
+  return r;
+}
+
+int main()
+{}
