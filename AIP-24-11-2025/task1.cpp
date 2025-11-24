1f448de3eee575e12b3bb12d229d8f2e5cf3ca4b
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-24 16:42:32 +0300

    added studDODEP

--- AIP-24-11-2025/task1.cpp
+++ AIP-24-11-2025/task1.cpp
@@ -12,8 +12,27 @@ size_t countYear(const Stud * const * studs, size_t k, int y)
   size_t r = 0;
   for (size_t i = 0; i < k; ++i) {
     const Stud * stud_i = studs[i];
-    int stud_y = stud_i -> year;
+    int stud_y = stud_i->year;
     r += (stud_y == y);
+  }
+  return r;
+}
+
+const Stud ** studDep(const Stud * const * studs, size_t k, int dep, size_t & found)
+{
+  size_t dep_count = 0;
+  for (size_t i = 0; i < k; ++i) {
+    const Stud * stud_i = studs[i];
+    dep_count += (stud_i->dep == dep);
+  }
+  const Stud ** r = new const Stud * [dep_count];
+  found = dep_count;
+  size_t j = 0;
+  for (size_t i = 0; i < k; ++i) {
+    const Stud * stud_i = studs[i];
+    if (stud_i->dep == dep) {
+      r[j++] = stud_i;
+    }
   }
   return r;
 }
