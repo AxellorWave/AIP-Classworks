08a9febeea31fd0b279ec9d3093045dfb089ab63
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-24 17:09:36 +0300

    maxVisits() visits() isBetter()

--- AIP-24-11-2025/task1.cpp
+++ AIP-24-11-2025/task1.cpp
@@ -7,6 +7,13 @@ struct Stud {
   int dep;
 };
 
+struct Subj {
+  const char * name;
+  size_t people, classes;
+  const Stud * const * studs;
+  const size_t * visited;
+};
+
 size_t countYear(const Stud * const * studs, size_t k, int y)
 {
   size_t r = 0;
@@ -39,6 +46,33 @@ const Stud ** studDep(const Stud * const * studs, size_t k, int dep, size_t & fo
 
 const Stud *** studDeps(const Stud * const * studs, size_t k, int max_dep, size_t ** found) 
 {}
+
+size_t maxVisits(const Subj & sub, int y)
+{
+  size_t cy = countYear(sub.studs, sub.people, y);
+  return cy * sub.classes;
+}
+
+size_t visits(const Subj & sub, int y)
+{
+  size_t r = 0;
+  for (size_t i = 0; i < sub.people; ++i) {
+    const Stud * stud_i = sub.studs[i];
+    if (stud_i->year == y) {
+      r += sub.visited[i];
+    }
+  }
+  return r;
+}
+
+bool isBetter(const Subj & sub, int y1, int y2)
+{
+  size_t n_y1 = visits(sub, y1);
+  size_t m_y1 = maxVisits(sub, y1);
+  size_t n_y2 = visits(sub, y2);
+  size_t m_y2 = maxVisits(sub, y2);
+  return n_y1 * m_y2 > n_y2 * m_y1; 
+}
 
 int main()
 {}
