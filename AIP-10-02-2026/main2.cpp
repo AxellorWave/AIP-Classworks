f68c474468724c53e3457584ff1e3ff337fc91ac
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-02-10 10:46:18 +0300

    quad_partition

--- AIP-10-02-2026/main2.cpp
+++ AIP-10-02-2026/main2.cpp
@@ -14,6 +14,32 @@ int * reverse(const int * a, size_t s){
   return res;
 }
 
+int * partition(int * a, size_t s, bool(*cond)(int))
+{
+  size_t ind = 0;
+  for (size_t i = 0; i < s; ++i) {
+    if (cond(a[i])) {
+      int e = a[i];
+      for (size_t j = i; j > ind; --j) {
+        a[j] = a[j - 1];
+      }
+      a[ind] = e;
+      ++ind;
+    }
+  }
+  return a + ind;
+}
+
+void quad_partition(int * a, size_t s, bool(*c1)(int), bool(*c2)(int), int ** pts)
+{
+  int * res1 = partition(a, s, c1);
+  int * res2 = partition(a, res1 - a, c2);
+  int * res3 = partition(res1, s - (res1 - a), c2);
+  pts[0] = res1;
+  pts[1] = res2;
+  pts[2] = res3;
+
+}
 
 int main()
 {
