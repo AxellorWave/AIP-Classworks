dd00b97c009bdb4010adf4ae308dd13925530fe5
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-02-17 09:40:40 +0300

    end of the lesson

--- AIP-17-02-2026/main.cpp
+++ AIP-17-02-2026/main.cpp
@@ -1,2 +1,27 @@
+#include <iostream>
+
+template <size_t N>
+struct Fibonachi {
+  enum {
+    value = Fibonachi< N-1>::value + Fibonachi<N-2>::value
+  };
+};
+
+template< >
+struct Fibonachi< 0 > {
+  enum {
+    value = 0
+  };
+};
+
+template< >
+struct Fibonachi< 1 > {
+  enum {
+    value = 1
+  };
+};
+
 int main()
-{}
+{
+  std::cout << Fibonachi< 10 >::value << "\n";
+}
