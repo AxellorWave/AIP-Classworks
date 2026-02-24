c3e1a0c89794adc862025e1e3674f65bd852bf05
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-02-24 08:43:15 +0300

    Iter for Vec

--- AIP-24-02-2026/main.cpp
+++ AIP-24-02-2026/main.cpp
@@ -1,2 +1,35 @@
-int main()
-{}
+template< class T >
+struct Vec {
+  T* data;
+  size_t s;
+};
+template< class T >
+struct VecIt {
+  T* data;
+  size_t s, i;
+};
+
+template< class T >
+VecIt< T > begin(Vec< T > v)
+{
+  return {v.data, v.s, 0};
+}
+
+template< class T >
+bool hasNext(VecIt< T > it)
+{
+  return it.s != it.i;
+}
+
+template< class T >
+VecIt< T > next(VecIt< T > it)
+{
+  it.i++;
+  return it;
+}
+
+template < class T >
+T & value(VecIt< T > it)
+{
+  return it.data[it.i];
+}
