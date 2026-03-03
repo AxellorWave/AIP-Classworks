43fecb39fbcfd42c6dd69b8c2598b0eac64c7fad
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-03 10:53:12 +0300

    copy was completed

--- AaDS-03-03-2026/main.cpp
+++ AaDS-03-03-2026/main.cpp
@@ -13,8 +13,70 @@ struct Vec {
 };
 
 template< class T >
+void clear(List< Vec< T > > * h)
+{
+  while (h) {
+    List< Vec< T > > * next = h->next;
+    delete[] h->val.data;
+    delete[] h;
+    h = next;
+  }
+
+}
+
+template< class T >
+Vec< T > copy(const Vec< T > & v)
+{
+  Vec< T > res;
+  res.s = v.s;
+  try {
+    res.data = new T[v.s];
+    for (size_t i = 0; i < v.s; ++i) {
+      res.data[i] = v.data[i];
+    }
+  } catch (...) {
+    delete[] res.data;
+  }
+  return res;
+}
+
+
+template< class T >
 List< Vec< T > > * copy(List< Vec< T > > * h)
-{}
+{
+  List< Vec< T > > * res = nullptr;
+  try {
+    res = new List< Vec< T > >;
+    List< Vec< T > > * c = res;
+    while (h) {
+      c->val = copy(h->val);
+      if (h->next) {
+        c->next = new List< Vec< T > >;
+      }
+      c = c->next;
+      h = h->next;
+    }
+  } catch (...) {
+    clear(res);
+    throw;
+  }
+  return res;
+}
 
 int main()
-{}
+{
+  List< Vec< int > >* h = new List<Vec<int>>;
+  h->val = Vec<int>{new int[3], 3};
+  for (size_t i = 0; i < 3; ++i) {
+    h->val.data[i] = (int) i;
+  }
+  h->next = new List<Vec<int>>;
+  h->next->val = Vec<int>{new int[3], 3};
+  for (size_t i = 3; i < 6; ++i) {
+    h->next->val.data[i-3] = (int) i;
+  }
+  List< Vec< int > >* r = copy(h);
+  clear(h);
+  clear(r);
+
+}
