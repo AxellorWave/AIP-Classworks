6a64c2f185a5236c0cc7554cdd49b868c9851bfe
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-03 10:08:47 +0300

    added signature

--- AaDS-03-03-2026/main.cpp
+++ AaDS-03-03-2026/main.cpp
@@ -1,2 +1,20 @@
+#include <iostream>
+
+template< class T >
+struct List {
+  T val;
+  List< T > * next;
+};
+
+template< class T >
+struct Vec {
+  T * data;
+  size_t s;
+};
+
+template< class T >
+List< Vec< T > > * copy(List< Vec< T > > * h)
+{}
+
 int main()
 {}
