21c89a92104f25d726b348aecb63ab3023b215df
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-04-07 10:58:09 +0300

    end with BiTree

--- AaDS-07-04-2026/main.cpp
+++ AaDS-07-04-2026/main.cpp
@@ -1,2 +1,131 @@
+#include <iostream>
+
+template< class T >
+struct BiTree {
+  T val;
+  BiTree< T > *lt, *rt, *parent;
+};
+
+template< class T >
+struct BiTreeIt {
+  BiTree< T > * curr;
+};
+
+template< class T >
+T & value(BiTreeIt< T > it)
+{
+  return it.curr->val;
+}
+
+template< class T >
+BiTreeIt< T > next(BiTreeIt< T > it)
+{
+  if (!it.curr) {
+    return;
+  }
+  BiTree< T > * next = it.curr;
+  if (next->rt) {
+    next = next->rt;
+    next = minimum(next);
+  } else {
+    BiTree< T > * parent = next->parent;
+    while (parent && parent->lt != next) {
+      next = parent;
+      parent = next->parent;
+    }
+    next = parent;
+  }
+  return {next};
+}
+
+template< class T >
+BiTreeIt< T > prev(BiTreeIt< T > it)
+{
+  if (!it.curr) {
+    return;
+  }
+  BiTree< T > * next = it.curr;
+  if (next->lt) {
+    next = next->lt;
+    next = maximum(next);
+  } else {
+    BiTree< T > * parent = next->parent;
+    while (parent && parent->rt != next) {
+      next = parent;
+      parent = next->parent;
+    }
+    next = parent;
+  }
+  return {next};
+}
+
+template< class T >
+BiTree< T > * minimum(BiTree< T > * root)
+{
+  if (!root) {
+    return root;
+  }
+  while (root->lt) {
+    root->lt;
+  }
+  return root;
+}
+
+template< class T >
+BiTree< T > * maximum(BiTree< T > * root)
+{
+  if (!root) {
+    return root;
+  }
+  while (root->rt) {
+    root->rt;
+  }
+  return root;
+}
+
+template< class T, class F >
+F traverse(BiTree< T > * root, F f)
+{
+  if (!root) {
+    return f;
+  }
+  BiTree< T > * next = minimum(root);
+  while (next) {
+    f(next->val);
+    if (next->rt) {
+      next = next->rt;
+      next = minimum(next);
+    } else {
+      BiTree< T > * parent = next->parent;
+      while (parent && parent->lt != next) {
+        next = parent;
+        parent = next->parent;
+      }
+      next = parent;
+    }
+  }
+  return f;
+}
+
+template< class T >
+bool hasNext(BiTreeIt< T > it)
+{
+  return next(it).curr;
+}
+
+template< class T >
+bool hasPrev(BiTreeIt< T > it)
+{
+  return prev(it).curr;
+}
+
+
+template< class T >
+struct TriTree {
+  T val1, val2;
+  TriTree< T > *lt, *rt, *mid, *parent;
+};
+
+
 int main()
 {}
